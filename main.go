b13b72aab8c68ae22989b4cc071567fe79eab8b2
ASproson <a.sproson@outlook.com>
2024-06-10 13:31:17 +0100

    Initializes base server

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux() // http request multiplexer (router)
+
+	// Catch all route to return 404 for any path
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	fmt.Printf("Listening on port %s", port)
+
+	log.Fatal(server.ListenAndServe())
+}
