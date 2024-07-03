671d5c110d755411f87f70af80c47ed0ca5178fa
ASproson <a.sproson@outlook.com>
2024-07-03 11:59:19 +0100

    Resolves health root

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func main() {
 
 	mux := http.NewServeMux() // http request multiplexer (router)
 
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot))) // Server files on root dir
+	fileServer := http.FileServer(http.Dir(filePathRoot))
+	mux.Handle("/app/*", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -22,4 +24,15 @@ func main() {
 	fmt.Printf("Listening on port %s", port)
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	const contentType = "text/plain; charset=utf-8"
+	const statusCode = 200
+	const bodyText = "OK"
+
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	w.Write([]byte(bodyText))
+
 }
